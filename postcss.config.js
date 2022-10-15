d667d95e61dd6af82a459d7ba08e31037e10811f
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-10-15 14:16:49 -0300

    Postcss configuration

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+const postcssPresetEnv = require("postcss-preset-env");
+
+module.exports = {
+  plugins: [
+    postcssPresetEnv({
+      stage: 1,
+    }),
+  ],
+};
