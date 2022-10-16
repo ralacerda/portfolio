3301c1961f9d71290f6cd303ef606c10c7c75f8d
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-10-16 18:24:42 -0300

    New Project Card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,88 @@
+<script setup>
+defineProps(["title", "imgUrl", "repoUrl", "siteUrl"]);
+</script>
+
+<template>
+  <div class="project">
+    <h3 class="title">
+      {{ title }}
+    </h3>
+
+    <div class="description">
+      <slot> </slot>
+      <div class="links">
+        <a v-if="siteUrl" :href="siteUrl" class="button-link">Live website</a>
+        <a :href="repoUrl" class="inline-link">Code repository</a>
+      </div>
+    </div>
+
+    <div class="preview-background">
+      <a href="https://ralacerda.github.io/coloquio-kuhn/">
+        <img :src="imgUrl" alt="Project Screenshot" class="project-preview" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.project {
+  margin-top: 4rem;
+  margin-bottom: 8rem;
+  --screenshot-offset: -12px, -12px;
+  display: grid;
+  grid-template-columns: 5fr 4fr;
+  grid-template-areas:
+    "title screenshot"
+    "description screenshot ";
+  align-items: start;
+  grid-template-rows: auto 1fr;
+  row-gap: 28px;
+  column-gap: 120px;
+}
+
+.project:nth-child(even) {
+  --screenshot-offset: 12px, -12px;
+  grid-template-columns: 4fr 5fr;
+  grid-template-areas:
+    "screenshot title"
+    "screenshot description";
+}
+
+.title {
+  grid-area: title;
+  text-align: center;
+}
+
+.preview-background {
+  background-color: var(--danger);
+  grid-area: screenshot;
+  align-self: start;
+}
+
+.project-preview {
+  box-shadow: 0px 0px 0px 0px rgb(0, 0, 0, 0);
+  transition: box-shadow 200ms, transform 200ms;
+}
+
+.project-preview:hover {
+  box-shadow: 3px 3px 6px 0 rgb(0, 0, 0, 0.2);
+  transform: translate(var(--screenshot-offset));
+}
+
+.links {
+  text-align: center;
+  margin-top: 2rem;
+}
+
+.links > * {
+  margin-right: 2rem;
+}
+
+.description {
+  grid-area: description;
+}
+
+.description > :deep(p) {
+  margin-bottom: 16px;
+}
+</style>
--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,11 +1,45 @@
 <script setup>
-import ProjectKuhn from "@/components/Projects/ProjectKuhn.vue";
+import ProjectCard from "./ProjectCard.vue";
+import kuhn from "@/assets/projects/kuhn.png";
+import f1next from "@/assets/projects/f1next.png";
 </script>
 
 <template>
+  <h2>Projects</h2>
   <div class="projects content">
-    <h2>Projects</h2>
-    <ProjectKuhn />
+    <ProjectCard
+      title="Thomas Kuhn Colloquium"
+      :imgUrl="kuhn"
+      siteUrl="https://ralacerda.github.io/coloquio-kuhn/"
+      repoUrl="https://github.com/ralacerda/coloquio-kuhn"
+      class="project"
+    >
+      <p>
+        Website created for the academic event Thomas Kuhn Colloquium. It
+        includes information about the event location, dates, guests, talks and
+        schedule.",
+      </p>
+      <p>
+        Built with <em>Eleventy</em>, <em>AlpineJS</em> and <em>Tailwind</em>,
+        hosted on <em>Github Pages</em>
+      </p>
+    </ProjectCard>
+
+    <ProjectCard
+      title="F1 Next"
+      :imgUrl="f1next"
+      repoUrl="https://github.com/ralacerda/f1next"
+      class="project"
+    >
+      <p>
+        A small script written in <em>Python</em> to display information about
+        the next Formula 1 Grand Prix from the terminal.
+      </p>
+      <p>
+        It includes flags for showing the full weekend schedule or a countdown
+        to the next event.
+      </p></ProjectCard
+    >
   </div>
 </template>
 
