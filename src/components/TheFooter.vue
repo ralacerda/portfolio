a342c9ee15d77d6c9593e1d13c83519f013f04fb
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-10-19 15:26:49 -0300

    Footer divider curves

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,55 +1,35 @@
-<script setup>
-import Github from "~icons/mdi/github";
-import Linkedin from "~icons/mdi/linkedin";
-import Twitter from "~icons/mdi/twitter";
-import Codepen from "~icons/mdi/codepen";
-</script>
+<script setup></script>
 
 <template>
-  <div class="footer">
-    <div class="footer__content content">
-      <div class="social-media">
-        <a href="#" class="social-media__link"><Github /></a>
-        <a href="#" class="social-media__link"><Linkedin /></a>
-        <a href="#" class="social-media__link"><Twitter /></a>
-        <a href="#" class="social-media__link"><Codepen /></a>
-      </div>
-      <div class="email">renato.ac.lacerda@gmail.com</div>
+  <footer class="footer">
+    <div style="overflow: hidden">
+      <svg
+        preserveAspectRatio="none"
+        viewBox="0 0 1200 120"
+        xmlns="http://www.w3.org/2000/svg"
+        style="width: 100%; height: 40px; transform: rotate(180deg) scaleX(-1)"
+        class="divider"
+      >
+        <path
+          d="M321.39 56.44c58-10.79 114.16-30.13 172-41.86 82.39-16.72 168.19-17.73 250.45-.39C823.78 31 906.67 72 985.66 92.83c70.05 18.48 146.53 26.09 214.34 3V0H0v27.35a600.21 600.21 0 00321.39 29.09z"
+        />
+      </svg>
     </div>
-  </div>
+  </footer>
 </template>
 
 <style>
 .footer {
-  background-color: var(--bg-secondary);
-  box-shadow: 0px 0 2px 0px #00000060;
+  margin-top: 4rem;
+  position: relative;
+  height: 1rem;
+  background: var(--fg-details);
+  line-height: 0;
 }
 
-.footer__content {
-  display: flex;
-  flex-direction: column;
-}
-
-.social-media {
-  margin: 0.7rem 0;
-  font-size: 2rem;
-  align-items: center;
-  justify-content: center;
-  display: flex;
-  gap: 1rem;
-}
-
-.social-media__link {
-  display: grid;
-  align-items: center;
-  color: var(--neutral-fg);
-}
-
-.social-media__link:hover {
-  color: var(--danger);
-}
-
-.email {
-  text-align: center;
+.divider {
+  inset: -40px 0 0 0;
+  position: absolute;
+  fill: var(--fg-details);
 }
 </style>
