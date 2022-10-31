e4acaf6c1b40e3bc317c6eec325663bde3426bfe
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-10-31 20:16:03 -0300

    Self-hosting Nunito Sans as is the only font

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import "@fontsource/nunito-sans/400.css";
+import "@fontsource/nunito-sans/600.css";
 import { ViteSSG } from "vite-ssg/single-page";
 import App from "./App.vue";
 import "./assets/main.css";
